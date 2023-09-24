8eeaf33d46a26ea6e86e362e931384a82717ba1e
Sena Nur Ertürk <se_na_e12@hotmail.com>
2023-09-24 13:52:50 +0300

    footer added

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,9 +1,11 @@
 import Image from "next/image"
 import Link from "next/link"
 
+import { footerLinks } from "@/constants";
+
 const Footer = () => {
     return (
-        <footer className="flex felx-col text-black-100 mt-5 border-t border-gray-100">
+        <footer className="flex flex-col text-black-100 mt-5 border-t border-gray-100">
             <div className="flex max-md:flex-col flex-wrap justify-between gap-5 sm:px-16 px-6 py-10">
                 <div className="flex flex-col justify-start items-start gap-6">
                     <Image
@@ -16,7 +18,35 @@ const Footer = () => {
                         All right reserved &copy;
                     </p>
                 </div>
-            </div>
+                <div className="footer__links">
+                    {footerLinks.map((footerLink) => (
+                        <div key={footerLink.title} className="footer__link">
+                            <h3 className="font-bold">{footerLink.title}</h3>
+                            {footerLink.links.map((item) => (
+                                <Link
+                                    key={item.title}
+                                    href={item.url}
+                                    className="text-gray-500"
+                                >
+                                    {item.title}
+                                </Link>
+                            ))}
+                        </div>
+                    ))}
+                </div>
+                </div>
+                <div className="flex justify-between items-center flex-wrap mt-10 border-t border-gary-100 sm:px-16 px-6 py-10">
+                   <p>@2023 Carhub. All Right Reserved</p>
+                    <div className="footer__copyrights-link">
+                        <Link href="/" className="text-gray-500">
+                            Privacy Pol
+                        </Link>
+                        <Link href="/" className="text-gray-500">
+                          Term of use
+                        </Link>
+                    </div>
+                </div>
+            
         </footer>
     )
 }
