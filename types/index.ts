2b319cf38f4bb0f76df68823131e7416e6c92a35
Sena Nur Ertürk <se_na_e12@hotmail.com>
2023-09-25 13:20:42 +0300

    SearchManufacturer added

--- types/index.ts
+++ types/index.ts
@@ -8,3 +8,7 @@ export interface CustomButtonProps{
     MouseEventHandler<HTMLButtonElement>
     btnType?: "button" | "submit";
 }
+export interface SearchManufacturerProps{
+    manufacturer:string;
+    setManufacturer:(manufacturer:string)=>void;
+}
